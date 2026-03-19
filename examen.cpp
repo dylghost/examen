026657a87c41a56591ad02bb4462f058016a6e5d
sebasghost <dylancito.cabrera15@gmail.com>
2026-03-18 20:42:02 -0500

    Empezamos el examen con un pequeño encabezado

---
+++ examen.cpp
@@ -0,0 +1,11 @@
+/*
+Nombre del estudiante: Dylan Sebastian Cabrera Enríquez
+Curso: Bootcamp Programación Cero a POO
+Clase: 10 - Examen 1 (C++)
+Fecha: 18/03/2026
+Tema:
+Sistema de registro y análisis de notas de estudiantes
+*/
+#include <iostream>
+using namespace std;
+
